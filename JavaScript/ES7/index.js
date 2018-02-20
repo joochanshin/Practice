b036a3467e46b6d7ebdd6d4d1b78805434bd2557
JooChan <14jshin@gmail.com>
2018-02-20 16:17:50 -0500

    playing with more functionality

--- JavaScript/ES7/index.js
+++ JavaScript/ES7/index.js
@@ -12,3 +12,51 @@ var Cat = function Cat() {
 };
 
 new Cat(); // => Tom true
+
+var _this = this;
+
+var fetchData = function fetchData() {
+  return regeneratorRuntime.async(function fetchData$(_context) {
+    while (1) {
+      switch (_context.prev = _context.next) {
+        case 0:
+          return _context.abrupt("return", fetch('https://randomuser.me/api/'));
+
+        case 1:
+        case "end":
+          return _context.stop();
+      }
+    }
+  }, null, _this);
+};
+
+var printData = function printData() {
+  var json;
+  return regeneratorRuntime.async(function printData$(_context2) {
+    while (1) {
+      switch (_context2.prev = _context2.next) {
+        case 0:
+          _context2.prev = 0;
+          _context2.next = 3;
+          return regeneratorRuntime.awrap(fetchData());
+
+        case 3:
+          json = _context2.sent;
+
+          console.log(json);
+          _context2.next = 10;
+          break;
+
+        case 7:
+          _context2.prev = 7;
+          _context2.t0 = _context2["catch"](0);
+
+          console.error("Problem", _context2.t0);
+
+        case 10:
+        case "end":
+          return _context2.stop();
+      }
+    }
+  }, null, _this, [[0, 7]]);
+};
--- React-Native/my-app/App.js
+++ React-Native/my-app/App.js
@@ -5,8 +5,12 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Hi Karen!</Text>
+        <Text>This is a test</Text>
+        <View style={styles1.box} />
+        <Text style={styles.text}>This is JooChan!</Text>
+
       </View>
+
     );
   }
 }
@@ -18,4 +22,74 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    backgroundColor: 'lightgrey',
+    color: '#4A90E2',
+    fontSize: 24,
+    padding: 10,
+  },
 });
+
+const styles1 = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  box: {
+    width: 200,
+    height: 200,
+    backgroundColor: 'lightblue',
+    borderWidth: 2,
+    borderColor: 'steelblue',
+    borderRadius: 20,
+  },
+})
+
+// import React, { Component } from 'react'
+// import { AppRegistry, View, Text, StyleSheet } from 'react-native'
+
+// class Counter extends Component {
+
+//   state = {count: 0}
+
+//   componentDidMount() {
+//     setInterval(() => {
+//       this.setState({count: this.state.count + 1})
+//     }, 1000)
+//   }
+
+//   render() {
+//     const {count} = this.state
+//     const {color, size} = this.props
+
+//     return (
+//       <Text style={{color, fontSize: size}}>
+//         {count}
+//       </Text>
+//     )
+//   }
+// }
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <Counter color={'lightblue'} size={16} />
+//         <Counter color={'skyblue'} size={32} />
+//         <Counter color={'steelblue'} size={80} />
+//         <Counter color={'darkblue'} size={140} />
+//       </View>
+//     )
+//   }
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//   },
+// })
+
+// AppRegistry.registerComponent('App', () => App)
