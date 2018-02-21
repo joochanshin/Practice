254345d6ce03e1a1e2310c92e1ba2a2d7d2e8cd3
JooChan <14jshin@gmail.com>
2018-02-21 15:14:43 -0500

    added basic functions and models

--- Rails/blogs/config/routes.rb
+++ Rails/blogs/config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  get '/users', to: "users#index"
+  get 'users/new'
+  post 'users', to: 'users#create'
+  get 'users/show'
+
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
+
+	root to: "blogs#index"
+  resources :blogs
 end
