39af9ce745adcd4a0a1feb8aa23f51aa64b43218
JooChan <14jshin@gmail.com>
2018-02-23 12:44:13 -0500

    added homepage

--- Rails/Overtime/spec/rails_helper.rb
+++ Rails/Overtime/spec/rails_helper.rb
@@ -19,7 +19,7 @@ RSpec.configure do |config|
   # instead of true.
   config.use_transactional_fixtures = false
 
-  config.before(:suite) { DatabaseCleaner.clean_with(:truncate) }
+  config.before(:suite) { DatabaseCleaner.clean_with(:truncation) }
   config.before(:each) { DatabaseCleaner.strategy = :transaction }
   config.before(:each, :js => true) { DatabaseCleaner.strategy = :truncation }
   config.before(:each) { DatabaseCleaner.start }
