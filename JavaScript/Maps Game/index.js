8ac7eeb557a796f2499d7b5d7e9b9f38abb0918d
JooChan <14jshin@gmail.com>
2018-04-03 02:28:05 -0400

    initial commit

--- JavaScript/Maps Game/index.js
+++ JavaScript/Maps Game/index.js
@@ -1,15 +1,36 @@
 console.log("This is a test");
 
-var states = ["Washington", "Oregon", "California", "Nevada", "Idaho", "Montana", "Wyoming", "Utah", "Arizona",
+const states = ["Washington", "Oregon", "California", "Nevada", "Idaho", "Montana", "Wyoming", "Utah", "Arizona",
 		"New Mexico", "Colorado", "North Dakota", "South Dakota", "Nebraska", "Kansas", "Oklahoma", "Texas",
 		"Minnesota", "Iowa", "Missouri", "Arkansas", "Louisiana", "Wisconsin", "Illinois", "Mississippi", "Alabama",
 		"Tennessee", "Kentucky", " Indiana", "Michigan", "Ohio", "West Virginia", "Florida", "Georgia", "South Carolina",
 		"North Carolina", "Virginia", "Maryland", "Delaware", "New Jersey", "Pennsylvania", "New York", "Connecticut",
 		"Rhode Island", "Massachusetts", "Vermont", "New Hampshite", "Maine", "Alaska", "Hawaii"];
+var correct = [];
 console.log(states.length);
 
 function check(){
 	var value = document.getElementById("input").value;
 	console.log(value);
+	console.log(states_check(value));
 	document.getElementById("input").value = "";
+}
+
+function states_check(value){
+	for(let i = 0; i < states.length; i++){
+		if(value.toLowerCase() === states[i].toLowerCase() && !already_guessed(value)){
+			// console.log(value, states[i], already_guessed());
+			correct.push(value);
+			return "You are correct";
+		}
+	}
+	return "Wrong or already guessed";
+}
+
+function already_guessed(value){
+	console.log("Length", correct.length);
+	for(let i = 0; i < correct.length; i++)
+		if(value.toLowerCase() === correct[i].toLowerCase())
+			return true;
+	return false;
 }
